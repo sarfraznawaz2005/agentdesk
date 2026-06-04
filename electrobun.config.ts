f1f0c87ddee949dcc1affd0b1cab3bbc8e806507
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2026-06-05 02:10:09 +0500

    chore: release v1.2.7

--- electrobun.config.ts
+++ electrobun.config.ts
@@ -4,7 +4,7 @@ export default {
 	app: {
 		name: "AgentDesk",
 		identifier: "com.sarfrazai.agentdesk",
-		version: "1.2.6",
+		version: "1.2.7",
 	},
 	build: {
 		// Vite builds to dist/, we copy from there
